bf0a619707118b0b3e8ba114e7a3754de4678da7
Joshi-Atharva <atharva.a.j234@gmail.com>
2024-12-30 22:36:12 +0530

    Add files via upload

---
+++ FFHQ model/ffhq_dataset.py
@@ -0,0 +1,58 @@
+# defining dataset class for the FFHQ dataset
+from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_image
+import os
+import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+DATASET_PATH = '/content/drive/My Drive/Datasets/FFHQ'
+TRAIN_SIZE = 900
+TEST_SIZE = 100
+NOISE_FACTOR = 0.1
+
+class FFHQDataSet(Dataset):
+    def __init__(self, dataset_path = DATASET_PATH, transform = None, noise_factor = NOISE_FACTOR):
+        self.dataset_path = dataset_path
+        self.transform = transform
+        self.noise_factor = noise_factor
+        self.len = len(os.listdir(dataset_path))
+    def __len__(self):
+        return self.len
+    def __getitem__(self, idx):
+        if(isinstance(idx, slice)):
+            return [self[ii] for ii in range(*idx.indices(len(self)))]
+        else:
+            name = self.name_constructor(idx)
+            img_path = os.path.join(self.dataset_path, name)
+            image = read_image(img_path)
+            if self.transform:
+                image = self.transform(image)
+            
+            input = image.clone() + torch.randn(image.shape)*self.noise_factor
+            target = image.clone()
+            input = input.to(device)
+            target = target.to(device)
+
+            return input, target
+    def name_constructor(self, idx):
+        leading_zeros = 5 - len(str(idx))
+        name = '0' * leading_zeros + str(idx) + '.png'
+        return name
+
+class TrainSet(FFHQDataSet):
+    def __init__(self, dataset_path = DATASET_PATH, transform = None, size = TRAIN_SIZE):
+        super().__init__(dataset_path, transform) # super() is used to refer to parent class. That way this line is calling the __init__ method of the parent class with specified parameters
+        self.size = size
+    def __len__(self):
+        return self.size
+    def __getitem__(self, idx):
+        return super().__getitem__(idx)
+
+class TestSet(FFHQDataSet):
+    def __init__(self, dataset_path = DATASET_PATH, transform = None, size = TEST_SIZE):
+        super().__init__(dataset_path, transform)
+        self.size = size
+    def __len__(self):
+        return self.size
+    def __getitem__(self, idx):
+        return super().__getitem__(idx)
--- FFHQ model/utils.py
+++ FFHQ model/utils.py
@@ -1,1 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+def PSNR(img1, img2):
+    mse = np.mean((img1 - img2) ** 2)
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 1
+    return 20 * np.log10(PIXEL_MAX / np.sqrt(mse))
+
+def imshow_side_by_side(input, target, prediction):
+    npimg_input = input.numpy()
+    npimg_target = target.numpy()
+    npimg_prediction = prediction.detach().numpy().squeeze(0)
+    print(npimg_prediction.shape)
+    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    axs[0].imshow(np.transpose(npimg_input, (1, 2, 0)))
+    axs[1].imshow(np.transpose(npimg_target, (1, 2, 0)))
+    axs[2].imshow(np.transpose(npimg_prediction, (1, 2, 0)))
+    plt.show()
+
+def predict(idx, model, device, test_set):
+    model.eval()
+    input, target = test_set[idx]
+    input = input.to(device)
+    target = target.to(device)
+    output = model(input.unsqueeze(0))
+    input = input.to('cpu')
+    target = target.to('cpu')
+    output = output.to('cpu')
+    imshow_side_by_side(input, target, output)
+    return output, target
+
+def test(model, device, test_set):
+    model.eval()
+    accuracy_aggregator = 0
+    for idx in range(len(test_set)):
+        output, target = predict(idx, model, device, test_set)
+        accuracy = PSNR(output.detach().cpu().numpy(), target.detach().cpu().numpy())
+        print(f'Accuracy: {accuracy:.0f} dB')
+        accuracy_aggregator += accuracy/len(test_set)
+    print(f'Test Accuracy: {accuracy_aggregator:.0f} dB')
+    return accuracy_aggregator
